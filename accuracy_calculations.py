d372a800f1ce530f0157867ec495713bd634700d
Christine <ctataru5@stanford.edu>
2018-06-06 19:10:11 -0700

    added calcError to calculate average num incorrect bases per base and per sequence

--- accuracy_calculations.py
+++ accuracy_calculations.py
@@ -13,3 +13,46 @@ def acc_sequences_correct(y_predicted, y_true):
             noiseless = noiseless + 1
             
     return noiseless/num_examples
+
+
+def calcError(y_predicted, y_true, X_data, verbose = False):
+   #Returns # of noisy nucleotides when comparing the predicted seqs and the target seqs perBase or perSequence. Error perBase should be interpreted as the probability that any given base output by the model is incorrect. Error perSequence should be interpreted as the average number of incorrect output bases per sequence. So an error of .03 means there at .03 incorrect bases in every sequence. This translates to 3 incorrect bases every 100 sequences. 
+
+    seq_lengths = [np.argmin(np.sum(X_data[i,:,:], axis = 1)) for i in range(X_data.shape[0])] #assumes 0000 for end of sequence. If there is no 0000, this will mess up, but I don't think that happens
+    output_dict = np.array(['A', 'C','G', 'T', '-'])
+    if verbose:
+        print(seq_lengths[0:10])
+    baseline_error = 0
+    pred_error = 0
+    bad_changes = 0
+    fail_to_change = 0
+    good_changes = 0
+    
+    for i in range(y_predicted.shape[0]):
+        target_char = output_dict[np.argmax(y_true[i,0:seq_lengths[i]], axis = -1)]
+        test_char = output_dict[np.argmax(X_data[i,0:seq_lengths[i]], axis = -1)]
+        pred_char = output_dict[np.argmax(y_predicted[i,0:seq_lengths[i]], axis = -1)]
+
+        baseline_error += np.sum(target_char != test_char) 
+        pred_error += np.sum(target_char != pred_char) 
+
+        to_red = np.where((target_char != pred_char) & (target_char == test_char))[0]
+        to_green = np.where((target_char != test_char) & (target_char == pred_char))[0]
+        to_blue = np.where((target_char != test_char) & (target_char != pred_char))[0]
+
+        bad_changes += len(to_red)
+        good_changes += len(to_green)
+        fail_to_change += len(to_blue)
+
+        norm_seq = y_predicted.shape[0]
+        norm_base = np.sum(seq_lengths)
+    if verbose:
+        print("Baseline error(num mismatched nucleotides between target and input average per sequence): " + str(baseline_error / norm_seq))
+        print("Prediction error(num mismatched nucleotides between target and prediction average per sequence): " + str(pred_error / norm_seq))
+        print( '\x1b[32m' + "Average good changes per sequence : " + '\x1b[0m'  + str(good_changes / norm_seq))
+        print('\x1b[31m' + "Average bad changes per sequence: " + '\x1b[0m' + str(bad_changes / norm_seq))
+        print('\x1b[34m' + "Average failure to change per sequence: "+ '\x1b[0m'  + str(fail_to_change / norm_seq)) 
+    
+    return(pred_error / norm_base, pred_error / norm_seq)
+
+
