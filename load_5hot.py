60b37803a2f5527551d2787cbad284663f7b8013
Christine <ctataru5@stanford.edu>
2018-10-24 12:39:42 -0700

    load_5hot outputs target strains for each set

--- load_5hot.py
+++ load_5hot.py
@@ -1,42 +1,36 @@
 import numpy as np
 
-def one_hot_sequence(sequence):
-    one_hot = np.zeros((1,5,len(sequence),1))
+def one_hot_sequence(s):
+    char_to_int = {'A': 0, 'C': 1, 'G': 2, 'T': 3, '-': 4}
+    s_num = [char_to_int[c] for c in s]
+    n_labels = 5
+    one_hot = np.eye(n_labels)[:,s_num]
+    return(one_hot) #convert to one-hot column-wise
 
-    for i in range(len(sequence)):
-        if sequence[i] == 'A':
-            one_hot[0,0,i,0] = 1
-        elif sequence[i] == 'C':
-            one_hot[0,1,i,0] = 1
-        elif sequence[i] == 'G':
-            one_hot[0,2,i,0] = 1
-        elif sequence[i] == 'T':
-            one_hot[0,3,i,0] = 1
-        else:
-        	one_hot[0,4,i,0] = 1
-
-    return one_hot
 
 def one_hot_list(entries,max_length):
     m = len(entries)
-    one_hot_ins = np.zeros((m,5,max_length,1))
-    one_hot_outs = np.zeros((m,5,max_length,1))
+    one_hot_ins = np.zeros((m,5,max_length))
+    one_hot_outs = np.zeros((m,5,max_length))
     print('Converting to one-hot...')
     
+    strains = []
     for i in range(0,m):
         entry = entries[i]
         
+        strains.append(entry[1])
         one_hot_in = one_hot_sequence(entry[2])
-        sequence_length = one_hot_in.shape[2]
+        sequence_length = one_hot_in.shape[1]
         one_hot_ins[i,:,0:sequence_length] = one_hot_in
         
         one_hot_out = one_hot_sequence(entry[3])
-        sequence_length = one_hot_out.shape[2]
+        sequence_length = one_hot_out.shape[1]
         one_hot_outs[i,:,0:sequence_length] = one_hot_out
         
             
     print('Done')
-    return one_hot_ins, one_hot_outs
+    return one_hot_ins, one_hot_outs, strains
+
 
 def load(datapath):
     print('Loading Data...')
@@ -82,9 +76,9 @@ def load(datapath):
     print('Maximum sequence length out is ' + str(max_length_out) + '\n')
     
     # CONVERT DATA OR SUBSET OF DATA TO ONE-HOT
-    [X_train, Y_train] = one_hot_list(train_entries, max(max_length_in,max_length_out))
-    [X_test, Y_test] = one_hot_list(test_entries, max(max_length_in,max_length_out))
-    [X_val, Y_val] = one_hot_list(val_entries, max(max_length_in,max_length_out))
+    [X_train, Y_train, target_strains_train] = one_hot_list(train_entries, max(max_length_in,max_length_out))
+    [X_test, Y_test, target_strains_test] = one_hot_list(test_entries, max(max_length_in,max_length_out))
+    [X_val, Y_val, target_strains_val] = one_hot_list(val_entries, max(max_length_in,max_length_out))
     
-    return X_train, Y_train, X_test, Y_test, X_val, Y_val
+    return X_train, Y_train, target_strains_train, X_test, Y_test, target_strains_test, X_val,Y_val, target_strains_val
 
